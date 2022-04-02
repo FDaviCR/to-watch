0f315500b91dc300c8d4c3a807be57a3ccd958e8
Davi <davicamiloribeiro@gmail.com>
2022-04-02 16:16:34 -0300

    Rotas adicionadas

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
-import * as ApiController from '../controllers/apiController';
+import * as ToWatchedController from '../controllers/towatchedController';
 
 const router = Router();
 
-
+router.get('/towatched', ToWatchedController.all);
+router.post('/towatched', ToWatchedController.add);
+router.put('/towatched/:id', ToWatchedController.update);
+router.delete('/towatched/:id', ToWatchedController.remove);
 
 export default router;
