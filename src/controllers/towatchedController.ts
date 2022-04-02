396435540dca75eb005806f43637d99d6b8d7de2
Davi <davicamiloribeiro@gmail.com>
2022-04-02 16:17:35 -0300

    Funções de crud

--- src/controllers/towatchedController.ts
+++ src/controllers/towatchedController.ts
@@ -5,3 +5,18 @@ export const ping = (req: Request, res: Response) => {
     res.json({pong: true});
 }
 
+export const all = (req: Request, res: Response) => {
+
+}
+
+export const add = (req: Request, res: Response) => {
+    
+}
+
+export const update = (req: Request, res: Response) => {
+    
+}
+
+export const remove = (req: Request, res: Response) => {
+    
+}
